72c94b8d3d5bf7f8fbc6554d16184a3cb4c1553f
Roxanne Weber <marie.sankofa@gmail.com>
2021-11-12 13:44:41 -0600

    all form pages now connect and working including postform locations home and success page

--- src/components/OrderForm.js
+++ src/components/OrderForm.js
@@ -1,17 +1,8 @@
 import React from 'react';
-//import { Link, useRouteMatch } from 'react-router-dom';
+
 import '../assets/css/Order.css';
 import '../assets/css/fonts.css';
 
-export default function OrderForm(props) {
-	return (
-		<div className='items-list-wrapper'>
-			<h1>Casey's Pizza Pub Order Form</h1>
-			<section id='form'>
-				<form id='pizza-form'></form>
-				<p>{props.locations.name}</p>
-				<button id='order-button'>Submit</button>
-			</section>
-		</div>
-	);
+export default function OrderForm() {
+	return <h1>hello</h1>;
 }
